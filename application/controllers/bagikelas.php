f0b211dba94ec37d4edbc1efc4b185d8d9200780
novanlexiku <novan.l@students.amikom.ac.id>
2020-02-03 13:41:47 +0700

    fix bagikelas

--- application/controllers/bagikelas.php
+++ application/controllers/bagikelas.php
@@ -1,4 +1,6 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class bagikelas extends CI_Controller{
   function __construct(){
     parent::__construct();
@@ -9,7 +11,7 @@ class bagikelas extends CI_Controller{
 
 
     $title = array(
-      'title' => 'Tabel bagikelas' ,
+      'title' => 'Tabel Bagikelas' ,
     );
     $x = array (
       'Total_rows' => $this->bagikelas_model->get_total(),
@@ -24,42 +26,185 @@ class bagikelas extends CI_Controller{
     header('Content-Type: application/json');
     echo $this->bagikelas_model->get_all_siswa();
   }
-  function get_kelas7a_json() { //data kelas1a by JSON object
+  //AMBIL DATA WALIKELAS 1 DARI MODEL
+  function get_kelas1a_json() { //data kelas1a by JSON object
     header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas7a();
+    echo $this->bagikelas_model->get_all_kelas1a();
   }
-  function get_kelas7b_json() { //data kelas1b by JSON object
+  function get_kelas1b_json() { //data kelas1b by JSON object
     header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas7b();
+    echo $this->bagikelas_model->get_all_kelas1b();
   }
-  function get_kelas7c_json() { //data kelas1c by JSON object
+  function get_kelas1c_json() { //data kelas1c by JSON object
     header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas7c();
+    echo $this->bagikelas_model->get_all_kelas1c();
   }
-  function get_kelas8a_json() { //data kelas2a by JSON object
+  function get_kelas1d_json() { //data kelas1a by JSON object
     header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas8a();
+    echo $this->bagikelas_model->get_all_kelas1d();
   }
-  function get_kelas8b_json() { //data kelas2b by JSON object
+  function get_kelas1e_json() { //data kelas1b by JSON object
     header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas8b();
+    echo $this->bagikelas_model->get_all_kelas1e();
   }
-  function get_kelas8c_json() { //data kelas2c by JSON object
+  function get_kelas1f_json() { //data kelas1c by JSON object
     header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas8c();
+    echo $this->bagikelas_model->get_all_kelas1f();
   }
-  function get_kelas9a_json() { //data kelas3a by JSON object
+  function get_kelas1g_json() { //data kelas1c by JSON object
     header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas9a();
+    echo $this->bagikelas_model->get_all_kelas1g();
   }
-  function get_kelas9b_json() { //data kelas3b by JSON object
-    header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas9b();
-  }
-  function get_kelas9c_json() { //data kelas3c by JSON object
-    header('Content-Type: application/json');
-    echo $this->bagikelas_model->get_all_kelas9c();
-  }
+
+//AMBIL DATA WALIKELAS 2 DARI MODEL
+function get_kelas2a_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas2a();
+}
+function get_kelas2b_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas2b();
+}
+function get_kelas2c_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas2c();
+}
+function get_kelas2d_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas2d();
+}
+function get_kelas2e_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas2e();
+}
+function get_kelas2f_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas2f();
+}
+function get_kelas2g_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas2g();
+}
+
+//AMBIL DATA WALIKELAS 3 DARI MODEL
+function get_kelas3a_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas3a();
+}
+function get_kelas3b_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas3b();
+}
+function get_kelas3c_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas3c();
+}
+function get_kelas3d_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas3d();
+}
+function get_kelas3e_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas3e();
+}
+function get_kelas3f_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas3f();
+}
+function get_kelas3g_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas3g();
+}
+
+//AMBIL DATA WALIKELAS 4 DARI MODEL
+function get_kelas4a_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas4a();
+}
+function get_kelas4b_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas4b();
+}
+function get_kelas4c_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas4c();
+}
+function get_kelas4d_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas4d();
+}
+function get_kelas4e_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas4e();
+}
+function get_kelas4f_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas4f();
+}
+function get_kelas4g_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas4g();
+}
+
+//AMBIL DATA WALIKELAS 5 DARI MODEL
+function get_kelas5a_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas5a();
+}
+function get_kelas5b_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas5b();
+}
+function get_kelas5c_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas5c();
+}
+function get_kelas5d_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas5d();
+}
+function get_kelas5e_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas5e();
+}
+function get_kelas5f_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas5f();
+}
+function get_kelas5g_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas5g();
+}
+
+//AMBIL DATA WALIKELAS 6 DARI MODEL
+function get_kelas6a_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas6a();
+}
+function get_kelas6b_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas6b();
+}
+function get_kelas6c_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas6c();
+}
+function get_kelas6d_json() { //data kelas1a by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas6d();
+}
+function get_kelas6e_json() { //data kelas1b by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas6e();
+}
+function get_kelas6f_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas6f();
+}
+function get_kelas6g_json() { //data kelas1c by JSON object
+  header('Content-Type: application/json');
+  echo $this->bagikelas_model->get_all_kelas6g();
+}
 
   function update(){ //function update data
     $kode=$this->input->post('kode_siswa');
--- application/models/bagikelas_model.php
+++ application/models/bagikelas_model.php
@@ -3,7 +3,8 @@ class bagikelas_model extends CI_Model{
 
   function get_total(){
     $this->db->select('*')
-    ->from('siswa');
+    ->from('Siswa')
+    ->where('siswa_role_id',3);
     return $this->db->count_all_results();
   }
 
@@ -23,166 +24,270 @@ class bagikelas_model extends CI_Model{
         ->add_column('view', '<a href="javascript:void(0);" rel="tooltip" title="Update Record" class="edit_record btn btn-success btn-simple btn-xs" data-kode="$1" data-nama="$2"  data-role="$3" data-kelas="$4"><i class="material-icons">create</i></a>','siswa_kode,siswa_nama,siswa_role_id,role_nama,siswa_kelas_id,kelas_nama');
         return $this->datatables->generate();
   }
-  function get_all_kelas7a() {
+
+  //AMBIL DATA bagikelas KELAS 1
+  function get_all_kelas1a() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',701);
+        ->where('siswa_kelas_id',101);
         return $this->datatables->generate();
   }
-  function get_all_kelas7b() {
+  function get_all_kelas1b() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',702);
+        ->where('siswa_kelas_id',102);
         return $this->datatables->generate();
   }
-  function get_all_kelas7c() {
+  function get_all_kelas1c() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',703);
+        ->where('siswa_kelas_id',103);
         return $this->datatables->generate();
   }
-  function get_all_kelas7d() {
+  function get_all_kelas1d() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',104);
+      return $this->datatables->generate();
+}
+function get_all_kelas1e() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',105);
+      return $this->datatables->generate();
+}
+function get_all_kelas1f() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',106);
+      return $this->datatables->generate();
+}
+function get_all_kelas1g() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',107);
+      return $this->datatables->generate();
+}
+
+  //AMBIL DATA bagikelas KELAS 2 
+  function get_all_kelas2a() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',704);
+        ->where('siswa_kelas_id',201);
         return $this->datatables->generate();
   }
-  function get_all_kelas7e() {
+  function get_all_kelas2b() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',705);
+        ->where('siswa_kelas_id',202);
         return $this->datatables->generate();
   }
-  function get_all_kelas7f() {
+  function get_all_kelas2c() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',706);
+        ->where('siswa_kelas_id',203);
         return $this->datatables->generate();
   }
-  function get_all_kelas7g() {
+  function get_all_kelas2d() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',204);
+      return $this->datatables->generate();
+}
+function get_all_kelas2e() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',205);
+      return $this->datatables->generate();
+}
+function get_all_kelas2f() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',206);
+      return $this->datatables->generate();
+}
+function get_all_kelas2g() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',207);
+      return $this->datatables->generate();
+}
+
+
+  //AMBIL DATA bagikelas KELAS 3
+  function get_all_kelas3a() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',707);
+        ->where('siswa_kelas_id',301);
         return $this->datatables->generate();
   }
-  function get_all_kelas7h() {
+  function get_all_kelas3b() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',708);
+        ->where('siswa_kelas_id',302);
         return $this->datatables->generate();
   }
-  function get_all_kelas7i() {
+  function get_all_kelas3c() {
         $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
         ->from('v_bagikelas')
-        ->where('siswa_kelas_id',709);
+        ->where('siswa_kelas_id',303);
         return $this->datatables->generate();
   }
-  function get_all_kelas8a() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',801);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas8b() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',802);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas8c() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',803);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas8d() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',804);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas8e() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',805);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas8f() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',806);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas8g() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',807);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas8h() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',808);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas8i() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',809);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9a() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',901);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9b() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',902);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9c() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',903);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9d() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',904);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9e() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',905);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9f() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',906);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9g() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',907);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9h() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',908);
-        return $this->datatables->generate();
-  }
-  function get_all_kelas9i() {
-        $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
-        ->from('v_bagikelas')
-        ->where('siswa_kelas_id',909);
-        return $this->datatables->generate();
-  }
+  function get_all_kelas3d() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',304);
+      return $this->datatables->generate();
+}
+function get_all_kelas3e() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',305);
+      return $this->datatables->generate();
+}
+function get_all_kelas3f() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',306);
+      return $this->datatables->generate();
+}
+function get_all_kelas3g() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',307);
+      return $this->datatables->generate();
+}
+
+//AMBIL DATA bagikelas KELAS 4
+function get_all_kelas4a() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',401);
+      return $this->datatables->generate();
+}
+function get_all_kelas4b() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',402);
+      return $this->datatables->generate();
+}
+function get_all_kelas4c() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',403);
+      return $this->datatables->generate();
+}
+function get_all_kelas4d() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',404);
+    return $this->datatables->generate();
+}
+function get_all_kelas4e() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',405);
+    return $this->datatables->generate();
+}
+function get_all_kelas4f() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',406);
+    return $this->datatables->generate();
+}
+function get_all_kelas4g() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',407);
+    return $this->datatables->generate();
+}
+
+//AMBIL DATA bagikelas KELAS 5
+function get_all_kelas5a() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',501);
+      return $this->datatables->generate();
+}
+function get_all_kelas5b() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',502);
+      return $this->datatables->generate();
+}
+function get_all_kelas5c() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',503);
+      return $this->datatables->generate();
+}
+function get_all_kelas5d() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',504);
+    return $this->datatables->generate();
+}
+function get_all_kelas5e() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',505);
+    return $this->datatables->generate();
+}
+function get_all_kelas5f() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',506);
+    return $this->datatables->generate();
+}
+function get_all_kelas5g() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',507);
+    return $this->datatables->generate();
+}
+
+//AMBIL DATA bagikelas KELAS 6
+function get_all_kelas6a() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',601);
+      return $this->datatables->generate();
+}
+function get_all_kelas6b() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',602);
+      return $this->datatables->generate();
+}
+function get_all_kelas6c() {
+      $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+      ->from('v_bagikelas')
+      ->where('siswa_kelas_id',603);
+      return $this->datatables->generate();
+}
+function get_all_kelas6d() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',604);
+    return $this->datatables->generate();
+}
+function get_all_kelas6e() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',605);
+    return $this->datatables->generate();
+}
+function get_all_kelas6f() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',606);
+    return $this->datatables->generate();
+}
+function get_all_kelas6g() {
+    $this->datatables->select('siswa_kode,siswa_nama,siswa_kelas_id,siswa_role_id,kelas_nama,role_nama')
+    ->from('v_bagikelas')
+    ->where('siswa_kelas_id',607);
+    return $this->datatables->generate();
+}
+
 }
